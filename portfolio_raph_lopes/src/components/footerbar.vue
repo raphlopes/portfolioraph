ccfbb1a1a51e022c857d3efef03593f8a970ea1c
Raphael <lopesraphael94@gmail.com>
2020-11-29 23:02:20 +0100

    création du footer

---
+++ portfolio_raph_lopes/src/components/footerbar.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div class="container">
+      <div class="links"><a href="#">About me</a><a href="#">Contact me</a><a href="#">Projects</a></div>
+      <div class="social-icons"><a href="#">
+        <i class="icon ion-social-facebook"></i></a>
+        <a href="https://instagram.com/raph_lop"><i class="icon ion-social-instagram-outline"></i></a>
+        <a href="#"><i class="icon ion-social-twitter"></i></a>
+      </div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+name: "footerbar"
+}
+</script>
+
+<style scoped>
+
+</style>
