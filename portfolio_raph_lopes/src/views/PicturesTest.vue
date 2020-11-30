f1c5b1b82c1475f5376278916c6e610c7ced620c
Senthan <senthan.28@gmail.com>
2020-11-30 23:05:35 +0100

    'non mais avec le css on peut rien faire de précis ' - Raph 2020

--- portfolio_raph_lopes/src/views/PicturesTest.vue
+++ portfolio_raph_lopes/src/views/PicturesTest.vue
@@ -1,44 +1,43 @@
 <template>
   <section>
-  <div class="row no-gutters">
-    <div class="col-md-6 col-lg-4 item zoom-on-hover">
-    <a>
-      <img class="img-fluid image" src="../assets/imgcover/japan.jpg">
-    </a>
-    </div>
-    <div class="col-md-6 col-lg-4 item zoom-on-hover">
-    <a>
-      <img class="img-fluid image" src="../assets/imgcover/greece.jpg">
-    </a>
-    </div>
-      <div class="col-md-6 col-lg-4 item zoom-on-hover">
-    <a>
-      <img class="img-fluid image" src="../assets/imgcover/nights.jpg">
-    </a>
+    <div class="row px-4" >
+      <div class="col-md-6 col-lg-4 item ">
+        <a class="">
+          <img class="img-fluid image w-100 h-100" src="../assets/imgcover/japan.jpg">
+        </a>
       </div>
-  </div>
-
+      <div class="col-md-6 col-lg-4 item">
+        <a>
+          <img class="img-fluid image w-100 h-100" src="../assets/imgcover/greece.jpg">
+        </a>
+      </div>
+      <div class="col-md-6 col-lg-4 item">
+        <a>
+          <img class="img-fluid image w-100 h-100" src="../assets/imgcover/nights.jpg">
+        </a>
+      </div>
+    </div>
+  
   </section>
 
 </template>
 
 <script>
-  
+
     export default {
         name: "PicturesTest",
-        components : {
-        
-        }
+        components: {}
     }
 </script>
 
 <style scoped>
-
-.project-covers{
-  display: flex;
-  flex-wrap: wrap;
-  padding-bottom: 0px;
-  padding-top: 0px;
-}
+  
+  .project-covers {
+    display: flex;
+    flex-wrap: wrap;
+    padding-bottom: 0px;
+    padding-top: 0px;
+  }
+  
 
 </style>
