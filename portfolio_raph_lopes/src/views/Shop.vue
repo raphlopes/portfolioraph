b8b83129f7c1aa2c145a5399c02f553f41a9de72
Raphael <lopesraphael94@gmail.com>
2020-12-05 17:12:17 +0100

    shop

--- portfolio_raph_lopes/src/views/Shop.vue
+++ portfolio_raph_lopes/src/views/Shop.vue
@@ -13,21 +13,21 @@
           <div class="col-md-6 col-lg-4">
             <div class="tuile-shop project-card-no-image">
               <h3>InPrint</h3>
-              <h4>You can shop my prints on InPrint here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="#">Shop</a>
+              <h4>You can shop my prints on InPrint here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="#" target="_blank">Shop</a>
             </div>
           </div>
 
           <div class="col-md-6 col-lg-4">
             <div class="tuile-shop project-card-no-image">
               <h3>DarkRoom</h3>
-              <h4>You can shop my prints on DarkRoom here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="#">Shop</a>
+              <h4>You can shop my prints on DarkRoom here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="https://lopesraphael.darkroom.tech/" target="_blank">Shop</a>
             </div>
           </div>
 
           <div class="col-md-6 col-lg-4">
             <div class="tuile-shop project-card-no-image">
               <h3>Society6</h3>
-              <h4>You can shop my prints on Society6 here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="#">Shop</a>
+              <h4>You can shop my prints on Society6 here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="#" target="_blank">Shop</a>
             </div>
           </div>
 
