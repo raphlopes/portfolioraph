359f4fe4131d6d0f0eb67519033f116988cabc11
HugoLougo <gohubb2@gmail.com>
2020-12-05 19:30:22 +0100

    envoi d'email asychrone

--- portfolio_raph_lopes/src/views/Contact.vue
+++ portfolio_raph_lopes/src/views/Contact.vue
@@ -8,39 +8,47 @@
         <div class="heading">
           <h2>Contact me</h2>
         </div>
-        <form method="get" action="formulaire-send-email.php" >
-          <div class="form-group"><label for="name">Votre nom</label><input class="form-control item" type="text" id="name"></div>
-          <div class="form-group"><label for="subject">Sujet</label><input class="form-control item" type="text" id="subject"></div>
-          <div class="form-group"><label for="email">Email</label><input class="form-control item" type="email" id="email"></div>
-          <div class="form-group"><label for="message">Message</label><textarea class="form-control item" id="message"></textarea></div>
-          <div class="form-group"><button onclick="sendEmail()" class="btn btn-primary btn-block btn-lg" type="submit">Envoi</button></div>
+        <form method="POST" action="https://formspree.io/f/xgepnweq">
+          <div class="form-group"><label for="name">Votre nom</label><input class="form-control item" type="text" name="name" id="name"></div>
+          <div class="form-group"><label for="subject">Sujet</label><input class="form-control item" type="text" name="subject" id="subject"></div>
+          <div class="form-group"><label for="email">Email</label><input class="form-control item" type="email" name="_replyto" id="email"></div>
+          <div class="form-group"><label for="message">Message</label><textarea class="form-control item" id="message" name="message"></textarea></div>
+          <div class="form-group"><button @click="sendEmail" id="sendEmail" class="btn btn-primary btn-block btn-lg" type="submit">Envoi</button></div>
         </form>
       </div>
     </section>
   </main>
 </template>
 
-<script src="https://smtpjs.com/v3/smtp.js"></script>
+
+
 <script>
+import axios from "axios";
+
   export default {
     name: "Contact",
-    methods : {
+    methods: {
       sendEmail: function () {
-        Email.send({
-          Host: "smtp.gmail.com",
-          Username: "gohubb2@gmail.com",
-          Password: "AsErftg,XOLJ74",
-          To: 'lopesraphy94@gmail.com',
-          From: "gohubb2@gmail.com",
-          Subject: "test d'email",
-          Body: "wesh bonjour test d'email",
-        }).then(
-            message => alert("mail sent successfully")
-        );
+        var message = "";
+        $("#sendMessage").on("click", function () {
+          message = $("#contactform").serialize();
+          $.ajax({
+            url: "//formspree.io/dynamicrealities@gmail.com",
+            method: "POST",
+            data: {message: message},
+            dataType: "json"
+          });
+          alert('Thanks for the email, we\'ll be in touch promptly.');
+          return false;
+        });
 
       }
+    },
+    mounted(){
+      axios.post("https://formspree.io/f/xgepnweq",{} ) //https://github.com/axios/axios
     }
   }
+
 
 
 </script>
