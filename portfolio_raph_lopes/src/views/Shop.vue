42bba87d6bd26b3cf96d47bf5f9d950a91518537
Raphael <lopesraphael94@gmail.com>
2021-04-10 12:06:08 +0200

    save

--- portfolio_raph_lopes/src/views/Shop.vue
+++ portfolio_raph_lopes/src/views/Shop.vue
@@ -6,10 +6,10 @@
 
         <div class="row">
 
-          <div class="col-md-6 col-lg-4">
+          <div class=" tuile col-md-6 col-lg-4">
             <div class="tuile-shop project-card-no-image">
               <in-prnt></in-prnt>
-              <h4>You can shop my prints on InPrint here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="#" target="_blank">Shop</a>
+              <h4>You can shop my prints on InPrint here</h4><a class="btn btn-outline-primary btn-sm shopping" role="button" href="https://inprnt.com/gallery/raph_lopes" target="_blank">Shop Now</a>
             </div>
           </div>
 
@@ -18,7 +18,7 @@
               <img class="fit-picture"
                    src="@/assets/icon/darkroom.png"
                    alt="darkroom">
-              <h4>You can shop my prints on DarkRoom here</h4><a class="btn btn-outline-primary btn-sm" role="button" href="https://lopesraphael.darkroom.tech/" target="_blank">Shop</a>
+              <h4>You can shop my prints on DarkRoom here</h4><a class="btn btn-outline-primary btn-sm shopping" role="button" href="https://lopesraphael.darkroom.tech/" target="_blank">Shop Now</a>
             </div>
           </div>
  <!--
@@ -50,16 +50,35 @@ name: "Shop",
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+.shopping{
+  background-color: white;
+  color: grey;
+  border-color: black;
+  border-width: thin;
+}
 
 
 .tuile-shop{
 
-
+  border-color: transparent;
   color: white;
   background-color: grey;
   border-radius: 5%;
+  height: auto;
+
+  svg {
+  width: 45%;
+    padding-bottom: 10%;
+    color:black;
+  }
+
+  img {
+    height: 65%;
+    width: 65%;
+    padding-bottom: 10%;
+  }
 
 }
 
